b0f09cbca271098ba764e028e8449de647f5b153
Haseeb-oss-eng <74820314+Haseeb-oss-eng@users.noreply.github.com>
2023-10-07 19:30:27 +0000

    completed
    this update

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
 #import flask
 from flask import Flask, render_template
 
-
+JOBS = [{
+  'id': 1,
+  'location': 'Bengaluru',
+  'Designation': 'Geospatial Engineer',
+  'Salary': 'Rs.45000'
+}, {
+  'id': 2,
+  'location': 'Chennai',
+  'Designation': 'Geospatial Developer',
+  'Salary': 'Rs.50000'
+}, {
+  'id': 3,
+  'location': 'Hyderabad',
+  'Designation': 'Geospatial Trainer',
+  'Salary': 'Rs.50000'
+}, {
+  'id': 4,
+  'location': 'Delhi',
+  'Designation': 'Drone Pilot',
+  'Salary': 'Rs.30000'
+}]
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_haseeb():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
 
 
 if __name__ == "__main__":
